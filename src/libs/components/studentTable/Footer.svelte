714ba471078444b5e0801c7187fae0199694af8d
BossNz <teerawatbossnz@gmail.com>
2023-12-27 19:42:39 +0700

    🔮 update: button export png

--- src/libs/components/studentTable/Footer.svelte
+++
@@ -1,3 +0,0 @@
-<footer class="text-end">
-    <p class="text-orange-300 text-xs">Powered by Computer Science, Kmitl</p>
-</footer>
--- src/pages/StudentTable.svelte
+++ src/pages/StudentTable.svelte
@@ -3,20 +3,19 @@
 
   import Head from "../libs/components/studentTable/Head.svelte";
   import ScheduleTable from "../libs/components/studentTable/ScheduleTable.svelte";
-  import Footer from "../libs/components/studentTable/Footer.svelte";
   import { toBlob } from "html-to-image";
 
   export let schedule: Array<ScheduleI>;
   export let information: InformationI;
 
   let captureScreen: any;
-  // setTimeout(async ()=>{
-  //   const blob = await toBlob(captureScreen)
-  //   if(blob == null) return
-  //   var file = new Blob([blob], { type: 'image/png' });
-  //   var fileURL = URL.createObjectURL(file);
-  //   window.open(fileURL);
-  // },1000)
+  const exportPng = async () => {
+    const blob = await toBlob(captureScreen);
+    if (blob == null) return;
+    var file = new Blob([blob], { type: "image/png" });
+    var fileURL = URL.createObjectURL(file);
+    window.open(fileURL);
+  };
 </script>
 
 <main class="min-h-screen p-2 flex flex-col justify-between">
@@ -26,5 +25,13 @@
       <ScheduleTable {schedule} />
     </div>
   </div>
-  <Footer/>
+  <footer class="text-end">
+    <button
+      on:click={exportPng}
+      class="rounded-full px-2 py-1 bg-orange-100 text-orange-500"
+    >
+      export png
+    </button>
+    <p class="text-orange-300 text-xs">Powered by Computer Science, Kmitl</p>
+  </footer>
 </main>
