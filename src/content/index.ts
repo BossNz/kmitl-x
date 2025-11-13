f4b993b7835c943cbc2c753be36cfec5c0890518
txasw <txasw@outlook.co.th>
2025-11-14 04:15:43 +0700

    feat: simulate the flow

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,11 +1,29 @@
 import { hideOverlay, showOverlay } from "./overlay";
 import { getCurrentRoute } from "./router";
 
+const KMITLX_MODE_KEY = "kmitlx:view";
+
 (async () => {
   showOverlay();
 
   const route = getCurrentRoute();
-  console.log("Current route:", route);
+  const mode = sessionStorage.getItem(KMITLX_MODE_KEY) ?? "new";
+  const isOriginalMode = mode === "original";
+
+  //   show original version without modifications
+  if (isOriginalMode) {
+    hideOverlay();
+    return;
+  }
+
+  // scrape the page content
+  const data = { error: "No scraper available for this page." };
+
+  if (data?.error) {
+    console.warn("[KMITL-X] Scraping error:", data.error);
+    hideOverlay();
+    return;
+  }
 
   await new Promise((resolve) => setTimeout(resolve, 2000));
   hideOverlay();
