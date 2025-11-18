341fdcbd4a36fdec209b03e3c5e67ded3cf77548
txasw <txasw@outlook.co.th>
2025-11-18 12:44:05 +0700

    fix: update term-selectors scraper to handle missing year and semester options

--- src/libs/scraper/term-selectors.scraper.ts
+++ src/libs/scraper/term-selectors.scraper.ts
@@ -3,16 +3,30 @@ import { BaseScraper } from "./baseScraper";
 
 export class TermSelectorsScraper extends BaseScraper {
   public async scrape(document: Document): Promise<TermSelectors> {
-    const yearOptions = this.getOptionsById(document, "year");
-    const semesterOptions = this.getOptionsById(document, "semester");
     return {
-      yearOptions,
-      semesterOptions,
+      yearOptions: this.getOptions(document, "year"),
+      semesterOptions: this.getOptions(document, "semester"),
     };
+  }
+
+  private getOptions(document: Document, query: string): string[] {
+    const select = this.getOptionsById(document, query);
+    if (select.length > 0) return select;
+    return this.getOptionsByName(document, query);
   }
 
   private getOptionsById(document: Document, id: string): string[] {
     const select = document.getElementById(id);
+    if (!select) return [];
+
+    const options = Array.from(
+      select.querySelectorAll<HTMLOptionElement>("option")
+    );
+    return options.map((option) => option.value);
+  }
+
+  private getOptionsByName(document: Document, name: string): string[] {
+    const select = document.getElementsByName(name)[0];
     if (!select) return [];
 
     const options = Array.from(
