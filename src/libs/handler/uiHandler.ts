e09c813b22ba82346dace3f9e007b02f2cfe81b0
txasw <txasw@outlook.co.th>
2025-11-21 01:12:01 +0700

    feat: inject Google Fonts styles for improved typography in mountUI function (temporary)

--- src/libs/handler/uiHandler.ts
+++ src/libs/handler/uiHandler.ts
@@ -26,6 +26,27 @@ export async function mountUI(
   // clear target
   target.innerHTML = "";
 
+  // inject font styles (Google Fonts - Prompt)
+  document.head.appendChild(
+    Object.assign(document.createElement("link"), {
+      rel: "preconnect",
+      href: "https://fonts.googleapis.com",
+    })
+  );
+  document.head.appendChild(
+    Object.assign(document.createElement("link"), {
+      rel: "preconnect",
+      href: "https://fonts.gstatic.com",
+      crossOrigin: "anonymous",
+    })
+  );
+  document.head.appendChild(
+    Object.assign(document.createElement("link"), {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap",
+    })
+  );
+
   // create shadow root
   const shadowContainer = document.createElement("div");
   shadowContainer.id = "kmitlx-root";
