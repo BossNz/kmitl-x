079f4d6b7ca61b13886e1aa163ccad300e925cc7
txasw <txasw@outlook.co.th>
2025-11-22 01:36:06 +0700

    feat: implement study table components and layout (bossnz design)

---
+++ src/libs/components/studytable/CardSubject.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import type { StudySchedule } from "../../types/report-studytable.types";
+  export let subject: StudySchedule;
+</script>
+
+<div
+  class="min-w-0 dark:bg-orange-500/10 bg-orange-100/50 border-l-2 dark:border-orange-400 border-orange-200 px-3 py-1 h-full flex flex-col justify-between rounded
+  hover:border-orange-500 dark:hover:border-orange-600 transition-all dark:text-orange-300 text-black"
+>
+  <!-- Top -->
+  <div class="flex justify-between text-xs opacity-65 z-10">
+    <!-- Type -->
+    <p class="truncate">
+      {subject.time[0]?.type === "lecture" ? "Lecture" : "Practical"}
+    </p>
+
+    <!-- Time -->
+    <p class="truncate">
+      {subject.time[0]?.startTime} - {subject.time[0]?.endTime}
+    </p>
+  </div>
+
+  <!-- Subject -->
+  <h3 class="text-orange-400 text-left text-sm truncate">
+    {subject.subjectName}
+  </h3>
+  <!-- Bottom -->
+  <div class="flex justify-between text-xs opacity-65">
+    <!-- Section -->
+    <p class="truncate">
+      section({subject.time[0]?.type === "lecture"
+        ? subject.lectureSection
+        : subject.practiceSection})
+    </p>
+
+    <!-- Location -->
+    <p class="truncate">
+      {subject.building}
+      {subject.room === "" ? "" : ` ( ${subject.room} )`}
+    </p>
+  </div>
+</div>
---
+++ src/pages/StudyTable.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import type { StudyTable } from "../libs/types/report-studytable.types";
+  import Head from "../libs/components/studytable/Head.svelte";
+  import ScheduleTable from "../libs/components/studytable/ScheduleTable.svelte";
+  import Icon from "@iconify/svelte";
+  import Button from "../libs/components/studytable/Button.svelte";
+
+  export let studentInfo: StudyTable["studentInfo"];
+  export let studySchedules: StudyTable["studySchedules"];
+
+  onMount(() => {
+    console.log("studentInfo:", studentInfo);
+    console.log("studySchedules:", studySchedules);
+  });
+</script>
+
+<main
+  class="min-h-screen p-2 flex flex-col justify-between dark:bg-gray-900 bg-white"
+>
+  <!-- Content -->
+  <div class="p-1 dark:bg-gray-900 bg-white">
+    <!-- Header -->
+    <Head {studentInfo} />
+
+    <!-- Schedule Table -->
+    <div class="mb-auto mt-5">
+      <ScheduleTable {studySchedules} />
+    </div>
+  </div>
+
+  <!-- Footer -->
+  <footer class="flex justify-between items-end">
+    <!-- Left -->
+    <div class="space-x-2">
+      <Button on:click>
+        <Icon icon="ph:file-png-light" class="my-auto text-2xl inline" />
+        <span class="font-semibold">Download PNG</span>
+      </Button>
+      <Button on:click>
+        <Icon icon="akar-icons:clipboard" class="my-auto text-2xl inline" />
+        <span class="font-semibold">Copy to Clipboard</span>
+      </Button>
+    </div>
+
+    <!-- Right -->
+    <div class="mt-auto text-right text-xs text-orange-300">
+      <p class="opacity-75">
+        Redesign by BossNz <Icon
+          icon="fluent-emoji:love-you-gesture"
+          class="inline"
+        />
+      </p>
+      <p>Powered by Computer Science, KMITL</p>
+    </div>
+  </footer>
+</main>
